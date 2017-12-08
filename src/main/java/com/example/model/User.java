588d4efaef1366feb30f92549bbe2cfe2720b897
bharadwaj0420 <bharadwaj0420@gmail.com>
2017-12-08 00:38:52 -0500

    sprinbbotmongo

---
+++ src/main/java/com/example/model/User.java
@@ -0,0 +1,41 @@
+package com.example.model;
+
+import org.springframework.data.annotation.Id;
+
+
+public class User {
+	
+	
+	@Id
+	private String Id;
+	private String name;
+	private int age;
+	private String email;
+	public String getId() {
+		return Id;
+	}
+	public void setId(String id) {
+		Id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+}
